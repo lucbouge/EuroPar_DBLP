0d706ef087c15f16bf7a69720de26fd4e0a6964b
Luc Bougé <Luc.Bouge@ens-rennes.fr>
2023-09-30 17:05:36 +0200

    Work

--- 2023-09-27/Phases/02_Make_stats/02_read_author_to_data_dict.py
+++ 2023-09-27/Phases/02_Make_stats/02_read_author_to_data_dict.py
@@ -3,5 +3,7 @@ from Libraries.Config.packages import *
 
 def main():
     author_to_data_dict = load_file_to_data(dataname="DBLP_author_to_data_dict")
+    print(f"{len(author_to_data_dict) = :_d}")
+    quit()
     for author, data in author_to_data_dict.items():
         pp.pprint((author, data))
