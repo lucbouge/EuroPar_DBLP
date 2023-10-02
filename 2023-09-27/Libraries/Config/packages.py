b9f040ec56be551f6f37615c8f33b91546d0f56d
Luc Bougé <Luc.Bouge@ens-rennes.fr>
2023-10-02 08:37:26 +0200

    Work

--- 2023-09-27/Libraries/Config/packages.py
+++ 2023-09-27/Libraries/Config/packages.py
@@ -38,11 +38,11 @@ from attrs import asdict, define, field, fields, frozen, validators
 from typeguard import typechecked, TypeCheckError, check_type
 
 
-from File.file_library import (
+from File import (
     dump_data_to_file,
     load_file_to_data,
     test_file_exists,
 )
-from Run.run_library import run 
+from Run import run
 
 from Libraries.Utilities.utilities import *
